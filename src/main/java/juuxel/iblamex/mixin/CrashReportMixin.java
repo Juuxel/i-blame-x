6d613f9ef7bbf35dbd80f86c5e9c8e98bab6b1d5
Juuxel <kasperi.kauppi@gmail.com>
2019-05-08 20:52:13 +0300

    Remove CrashReportSectionMixin

--- src/main/java/juuxel/iblamex/mixin/CrashReportMixin.java
+++ src/main/java/juuxel/iblamex/mixin/CrashReportMixin.java
@@ -1,23 +1,52 @@
 package juuxel.iblamex.mixin;
 
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.util.Lazy;
+import net.fabricmc.loader.api.ModContainer;
 import net.minecraft.util.crash.CrashReport;
+import net.minecraft.util.crash.CrashReportSection;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Mixin(CrashReport.class)
 public class CrashReportMixin {
-    private static final Lazy<String[]> I_BLAME_X$WITTY_COMMENTS = new Lazy<>(
-            () -> FabricLoader.getInstance().getAllMods()
-                    .stream()
-                    .map(mod -> String.format("I blame %s.", mod.getMetadata().getName()))
-                    .toArray(String[]::new)
-    );
+    @Shadow
+    @Final
+    private CrashReportSection systemDetailsSection;
+
+    @Unique
+    private static Stream<ModContainer> i_blame_x$getMods() {
+        return FabricLoader.getInstance().getAllMods().stream();
+    }
 
     @ModifyVariable(method = "generateWittyComment", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/SystemUtil;getMeasuringTimeNano()J", shift = At.Shift.BEFORE), ordinal = 0)
     private static String[] getWittyComments(String[] original) {
-        return I_BLAME_X$WITTY_COMMENTS.get();
+        return i_blame_x$getMods()
+                .map(mod -> String.format("I blame %s.", mod.getMetadata().getName()))
+                .toArray(String[]::new);
+    }
+
+    @Inject(method = "fillSystemDetails", at = @At(value = "INVOKE", ordinal = 1, target = "Lnet/minecraft/util/crash/CrashReportSection;add(Ljava/lang/String;Lnet/minecraft/util/crash/ICrashCallable;)V", shift = At.Shift.BEFORE))
+    private void onFillSystemDetailsAfterGameVersion(CallbackInfo info) {
+        systemDetailsSection.add(
+                "Mods",
+                i_blame_x$getMods().map(ModContainer::getMetadata)
+                        .map(meta -> String.format("%s@%s", meta.getId(), meta.getVersion()))
+                        .collect(Collectors.joining(", "))
+        );
+    }
+
+    @Inject(method = "asString", at = @At("RETURN"), cancellable = true)
+    private void onAsString(CallbackInfoReturnable<String> info) {
+        info.setReturnValue(info.getReturnValue() + "\n\tBlaming and listing mods brought to you by I Blame X.");
     }
 }
